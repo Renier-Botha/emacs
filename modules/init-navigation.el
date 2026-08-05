dc343d314cd05c1017f03286b4da7ad9fdcb0aca
Renier Botha <renierb00@gmail.com>
2026-08-05 19:40:59 +0200

    Added Consult for quick searching

---
+++ modules/init-navigation.el
@@ -0,0 +1,32 @@
+;;; init-navigation.el --- Consult -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; - consult: enhanced search/navigation commands (buffers, grep, lines...).
+
+;;; Code:
+
+(use-package consult
+  :bind (("C-x b"   . consult-buffer)
+         ("C-x 4 b" . consult-buffer-other-window)
+         ("C-x p b" . consult-project-buffer)
+         ("M-y"     . consult-yank-pop)
+         ("M-g g"   . consult-goto-line)
+         ("M-g M-g" . consult-goto-line)
+         ("M-g i"   . consult-imenu)
+         ("M-g o"   . consult-outline)
+         ("M-s l"   . consult-line)
+         ("M-s g"   . consult-ripgrep)
+         ("M-s f"   . consult-find)
+         :map minibuffer-local-map
+         ("M-s" . consult-history)
+         ("M-r" . consult-history))
+  :custom
+  ;; Don't preview expensive commands automatically.
+  (consult-preview-key 'any)
+  (register-preview-function #'consult-register-format)
+  :init
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref))
+
+(provide 'init-navigation)
+;;; init-navigation.el ends here
