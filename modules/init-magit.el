d347d049b1c1f0c8f3a7d1714428f04a152c0140
Renier Botha <renierb00@gmail.com>
2026-08-06 12:42:40 +0200

    Add diff-hl package

--- modules/init-magit.el
+++ modules/init-magit.el
@@ -13,5 +13,16 @@
   (setq magit-diff-refine-hunk 'all
         magit-save-repository-buffers 'dontask))
 
+;; Show added/removed/modified line markers in the fringe, updated live
+;; against the current git HEAD (i.e. relative to your current branch).
+(use-package diff-hl
+  :demand t
+  :config
+  (global-diff-hl-mode 1)
+  ;; Refresh the markers after Magit operations (commit, stage, etc.)
+  ;; since those don't otherwise trigger `after-save-hook'.
+  (add-hook 'magit-pre-refresh-hook #'diff-hl-magit-pre-refresh)
+  (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh))
+
 (provide 'init-magit)
 ;;; init-magit.el ends here
